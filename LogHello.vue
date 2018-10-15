35f4f57c8999d27e8e79a078e48869f2ed957df3
liumapp <liumapp.com@gmail.com>
2018-10-15 16:20:47 +0800

    add log hello vue component

---
+++ LogHello.vue
@@ -0,0 +1,17 @@
+/**
+ * @file LogHello.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/10/15
+ */
+<template>
+  <div>
+    this is log hello component
+  </div>
+</template>
+<script>
+export default {
+  name: 'logHello'
+}
+</script>
